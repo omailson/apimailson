1e6460aed41360fc1e9c3cdf5bf4e72565385b7d
Mailson Lira <mailson@gmail.com>
2011-11-11 01:41:13 -0300

    Convert rendered calendar to PDF
    We use inkscape to perform the conversion.
    
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('calendarapp.views',
     url(r'^(?P<name>(\w+))(?P<year>([0-9]{4}))(?P<month>([0-9]{2})).svg$', 'render', name='render'),
+    url(r'^(?P<name>(\w+))(?P<year>([0-9]{4}))(?P<month>([0-9]{2})).pdf$', 'renderpdf', name='renderpdf'),
 )
--- calendarapp/views.py
+++ calendarapp/views.py
@@ -10,3 +10,24 @@ def render(request, name, year, month):
     dom = render_calendar(xml.dom.minidom.parse(filename), int(month), int(year))
 
     return HttpResponse(dom.toxml().encode('utf8'), mimetype="image/svg+xml")
+
+def renderpdf(request, name, year, month):
+    import subprocess
+
+    filename = os.path.join(APP_ROOT, "static/calendars/%s.svg" % name)
+    dom = render_calendar(xml.dom.minidom.parse(filename), int(month), int(year))
+
+    svghandle = open("tmp.svg", "w")
+    svghandle.write(dom.toxml().encode("utf8"))
+    svghandle.close()
+
+    retcode = subprocess.call(['inkscape', '-A', 'tmp.pdf', 'tmp.svg'])
+
+    if retcode == 0:
+        pdfhandle = open("tmp.pdf", "r")
+        pdfcontent = pdfhandle.read()
+        pdfhandle.close()
+
+        return HttpResponse(pdfcontent, mimetype="application/pdf")
+    else:
+        return HttpResponse(":(")
