4ab246361b38caebdf60986f039b225fbfd1887a
Mailson Lira <mailson@gmail.com>
2011-11-11 01:35:58 -0300

    Renders a calendar given a svg file
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('calendarapp.views',
-    url(r'^$', 'home'),
+    url(r'^(?P<name>(\w+))(?P<year>([0-9]{4}))(?P<month>([0-9]{2})).svg$', 'render', name='render'),
 )
--- calendarapp/views.py
+++ calendarapp/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+from calendario import render_calendar
+import xml.dom.minidom
+import os.path
+
+APP_ROOT=os.path.realpath(os.path.dirname(__file__))
+
+def render(request, name, year, month):
+    filename = os.path.join(APP_ROOT, "static/calendars/%s.svg" % name)
+    dom = render_calendar(xml.dom.minidom.parse(filename), int(month), int(year))
+
+    return HttpResponse(dom.toxml().encode('utf8'), mimetype="image/svg+xml")
