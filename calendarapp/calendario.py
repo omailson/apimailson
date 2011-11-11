29c959b556499c4905bd61a293a8259724d24900
Mailson Lira <mailson@gmail.com>
2011-11-10 21:47:14 -0300

    Translate calendar script to english
    No refactor at all. Just translation.
    
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- calendarapp/calendario.py
+++ calendarapp/calendario.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-# -*- encoding: UTF-8 -*-
+# -*- encoding: utf-8 -*-
 
 import calendar
 import xml.dom.minidom
@@ -7,59 +7,59 @@ import re
 import datetime
 import glob
 
-meses = ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-anoHoje = datetime.date.today().year
-mesHoje = datetime.date.today().month
-ano = raw_input('Digite o ano [%d]:' % (anoHoje))
-if ano.isdigit():
-    ano = int(ano)
+yearToday = datetime.date.today().year
+monthToday = datetime.date.today().month
+year = raw_input('Year [%d]:' % (yearToday))
+if year.isdigit():
+    year = int(year)
 else:
-    ano = anoHoje
-mes = raw_input('Digite o mês [%d]:' % (mesHoje))
-if mes.isdigit():
-    mes = int(mes)
+    year = yearToday
+month = raw_input('Month [%d]:' % (monthToday))
+if month.isdigit():
+    month = int(month)
 else:
-    mes = mesHoje
+    month = monthToday
 
-listaDeArquivos = glob.glob('*.svg')
-if len(listaDeArquivos) == 0:
-    arquivo = raw_input('Digite o nome do arquivo de origem: ')
+filesList = glob.glob('*.svg')
+if len(filesList) == 0:
+    fileHandle = raw_input('Insert calendar template: ')
 else:
     i = 0
-    print 'Escolha o arquivo de origem: '
-    for j in listaDeArquivos:
+    print 'Choose calendar template: '
+    for j in filesList:
         i = i + 1
         print i, j
-    arquivo = listaDeArquivos[input('Escolha: ') - 1]
+    fileHandle = filesList[input('Choice: ') - 1]
 
 
 calendar.setfirstweekday(calendar.SUNDAY)
-calendario = calendar.monthcalendar(ano, mes)
+calendario = calendar.monthcalendar(year, month)
 
-dom = xml.dom.minidom.parse(arquivo)
+dom = xml.dom.minidom.parse(fileHandle)
 
-pattern = re.compile("s([1-6])d([1-7])")
+pattern = re.compile("w([1-6])d([1-7])")
 
-for nos in dom.getElementsByTagName('text'):
-    mat = pattern.match(nos.attributes["id"].value)
-    if nos.attributes["id"].value == 'mes':
-        nos.firstChild.firstChild.data = meses[mes - 1]
-    elif nos.attributes["id"].value == 'ano':
-        nos.firstChild.firstChild.data = ano
+for nodes in dom.getElementsByTagName('text'):
+    mat = pattern.match(nodes.attributes["id"].value)
+    if nodes.attributes["id"].value == 'month':
+        nodes.firstChild.firstChild.data = months[month - 1]
+    elif nodes.attributes["id"].value == 'year':
+        nodes.firstChild.firstChild.data = year
     elif mat != None:
-        s = int(mat.group(1)) - 1
+        w = int(mat.group(1)) - 1
         d = int(mat.group(2)) - 1
-        if s < len(calendario):
-            if calendario[s][d] == 0:
-                nos.firstChild.firstChild.data = ''
+        if w < len(calendario):
+            if calendario[w][d] == 0:
+                nodes.firstChild.firstChild.data = ''
             else:
-                nos.firstChild.firstChild.data = calendario[s][d]
+                nodes.firstChild.firstChild.data = calendario[w][d]
         else:
-            nos.firstChild.firstChild.data = ''
+            nodes.firstChild.firstChild.data = ''
 
-arquivoDestino = raw_input('Digite o nome do arquivo de destino: ')
-handleDestino = open(arquivoDestino, 'w')
-handleDestino.write(dom.toxml().encode('UTF-8'))
-handleDestino.close()
-print 'Salvo!'
+targetFile = raw_input('Insert target file name: ')
+targetHandle = open(targetFile, 'w')
+targetHandle.write(dom.toxml().encode('UTF-8'))
+targetHandle.close()
+print 'Done!'
